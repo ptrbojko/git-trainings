b38c345a9647660243d5cfc02db02ebe4f39d530
Piotr Bojko <ptr.bojko@gmail.com>
2014-08-18 12:47:47 +0200

    ignore

---
+++ src/main/resources/sql/Employee.sql
@@ -0,0 +1,15 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  pbojko
+ * Created: Sep 20, 2015
+ */
+
+CREATE TABLE Employees
+( employee_id number(10) NOT NULL,
+  employee_first_name varchar2(50) NOT NULL,
+  employee_last_name varchar2(50)
+);
